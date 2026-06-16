b8c8f4faef5b37f25253d08d89a6d66bbab3f1f8
christoferoAngelo <christoferoangelo@gmail.com>
2026-06-16 14:39:25 -0300

    Inicializando packages e pastas

---
+++ backend/src/main/java/com/eva/locafesta/LocafestaApplication.java
@@ -0,0 +1,13 @@
+package com.eva.locafesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocafestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocafestaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eva/locafesta/LocafestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eva.locafesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocafestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
